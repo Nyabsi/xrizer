8c43a56ad5bca5fb4f7aa971793b8058ed6a4ac6
Nyabsi <nyabsi@sovellus.cc>
2025-04-15 18:20:54 +0300

    i wrote rust last time like 2 years ago

--- src/input/skeletal.rs
+++ src/input/skeletal.rs
@@ -262,12 +262,12 @@ impl<C: openxr_data::Compositor> Input<C> {
             };
 
             *curl_value = curl;
-            let splay_angle = if finger == 0 { // Index
-                -0.05f32 + (((1.0f - curl) * 0.75) * 0.1f32)
-            } else if finger == 3 { // Pinky
-                0.08f32 + (((1.0f - curl) * 0.75) * 0.12f32)
+            let splay_angle = if i == 0 { // Index
+                -0.05 + (((1.0f - curl) * 0.75) * 0.1)
+            } else if i == 3 { // Pinky
+                0.08 + (((1.0f - curl) * 0.75) * 0.12)
             } else { // Middle and ring
-                ((1.0f - curl) * 0.75) * 0.07f32
+                ((1.0f - curl) * 0.75) * 0.07
             };
             finger_splay[i] = splay_angle;
         }
